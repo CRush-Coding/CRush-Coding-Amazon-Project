1a80074d1fc337591d1248cbe9ac504ba4cbb675
CRush-Coding <blazeboy2213@gmail.com>
2024-11-25 01:28:06 +1100

    Added Cart/delete functionality to checkout section

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,51 @@
-export const cart = [];
+export let cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity: 2,
+}, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1,
+}];
+
+
+
+export function addToCart(productId) {
+    let matchingItem;
+
+    cart.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+            matchingItem = cartItem;
+        }
+
+    });
+
+    const quantityValue = 
+    Number(document.querySelector(`.js-dropdown-quantity-${productId}`).value);
+    
+    if(matchingItem) {
+        matchingItem.quantity += quantityValue;
+    } else {
+        cart.push({
+            productId: productId,
+            quantity: quantityValue,
+        });
+
+    }
+}
+
+export function removeFromCart(productId) {
+    const newCart = [];
+
+    cart.forEach((cartItem) => {
+        if (cartItem.productId !== productId) {
+            newCart.push(cartItem);
+
+
+        }
+    });
+
+    console.log(newCart);
+
+    cart = newCart;
+}
+
+// console.log(removeFromCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6'));
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,5 +1,6 @@
-import {cart} from '../data/cart.js';
+import {cart, addToCart} from '../data/cart.js';
 import { products } from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
 
 let productsHTML = '';
 
@@ -24,7 +25,7 @@ products.forEach((product) => {
             </div>
 
             <div class="product-price">
-                ${(product.priceCents / 100).toFixed()}
+                ${formatCurrency(product.priceCents)}
             </div>
 
             <div class="product-quantity-container">
@@ -74,33 +75,12 @@ function updateCartQuantity() {
 }
 
 
-function addToCart(productId) {
-    let matchingItem;
-
-    cart.forEach((item) => {
-        if (productId === item.productId) {
-            matchingItem = item;
-        }
-
-    });
-
-    const quantityValue = 
-    Number(document.querySelector(`.js-dropdown-quantity-${productId}`).value);
-    
-    if(matchingItem) {
-        matchingItem.quantity += quantityValue;
-    } else {
-        cart.push({
-            productId: productId,
-            quantity: quantityValue,
-        });
-
-    }
-}
 
 
 
-document.querySelectorAll('.js-add-to-cart').forEach((button) => {button.addEventListener('click', 
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => 
+    {button.addEventListener('click', 
     () => {
         const productId = button.dataset.productId;
 
